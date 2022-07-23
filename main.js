1c08882d28271b31d8e0b6c67ed50afdb33240cc
Curtis Simpson <crsimpson5@gmail.com>
2022-07-22 19:03:36 -0500

    Add Swiper.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  grabCursor: true,
+  initialSlide: 2,
+  spaceBetween: 15,
+  breakpoints: {
+    768: {
+      spaceBetween: 30
+    }
+  }
+});
+
+const prevSlideBtn = document.querySelector(".button--control.prev");
+const nextSlideBtn = document.querySelector(".button--control.next");
+
+prevSlideBtn.addEventListener("click", () => {
+  swiper.slidePrev();
+});
+nextSlideBtn.addEventListener("click", () => {
+  swiper.slideNext();
+});
